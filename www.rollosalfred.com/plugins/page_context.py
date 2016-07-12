79feb4c841f50e57fe258b3c7b2eef0906338994
Gabriel Reges <gregesf@bluetideconsulting.com>
2016-07-12 10:48:01 -0500

    cambios en páginas de productos, credas galerías

--- www.rollosalfred.com/plugins/page_context.py
+++ www.rollosalfred.com/plugins/page_context.py
@@ -1,4 +1,15 @@
 #coding:utf-8
+def cine_gallery():
+    return ["%.2d" % i for i in range(1, 30)]
+
+def bn_gallery():
+    return ["%.2d" % i for i in range(1, 42)]
+
+def aom_gallery():
+    return ["%.2d" % i for i in range(1, 27)]
+
+def reserva_gallery():
+    return ["%.2d" % i for i in range(1, 38)]
 
 def preBuildPage(page, context, data):
     """
@@ -9,7 +20,11 @@ def preBuildPage(page, context, data):
     # Any changes you make to context will be passed to the template renderer for this page.
 
     extra = {
-        "CURRENT_PAGE": page
+        "CURRENT_PAGE": page,
+        "CINE_GALLERY": cine_gallery(),
+        "BN_GALLERY": bn_gallery(),
+        "AOM_GALLERY": aom_gallery(),
+        "RESERVA_GALLERY": reserva_gallery()
         # Add your own dynamic context elements here!
     }
 
